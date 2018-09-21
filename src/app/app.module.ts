adf2882143aab2ff5c585b981007e83bab2eb6ac
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-09-21 10:24:42 +0300

    add all the angular files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { UserRequestApiService } from './user-http/user-request-api.service';
 
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserRequestApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
