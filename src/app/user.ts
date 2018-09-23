28d35539db6cc1b4a7b890df84205a7650cf4585
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-09-23 21:01:50 +0300

    add html template for search form and results display

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,3 +1,3 @@
 export class User {
-    constructor(public user:string, public link:string, public repos:number){}
+    constructor(public user:string, public login:string, public bio:string, public link:string, public imageLink:string){}
 }
