64175656059679b66c09919f76cb3851ab15ddc2
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-09-24 13:10:02 +0300

    add search repos component

---
+++ src/app/search-repos/search-repos.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { UserRequestApiService } from '../user-http/user-request-api.service';
+
+
+@Component({
+  selector: 'app-search-repos',
+  templateUrl: './search-repos.component.html',
+  providers: [UserRequestApiService],
+  styleUrls: ['./search-repos.component.css']
+})
+export class SearchReposComponent implements OnInit {
+
+    repos:any[];
+    searchTerm:string;
+
+    constructor(private searchApi:UserRequestApiService) { }
+
+    findRepos (searchTerm:string) {
+        this.searchApi.updateSearchTerm(this.searchTerm);
+        this.searchApi.searchRepos();
+        this.repos = this.searchApi.reposArray;
+    }
+
+  ngOnInit() {
+  }
+
+}
