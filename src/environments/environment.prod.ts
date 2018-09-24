b5f835abc634d4a5f6d01cede55c326cbb48b7e2
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-09-24 16:37:19 +0300

    change gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:"https://api.github.com",
+  clientId:"abeb90549ecafe81772b",
+  clientSecret: "3b0ba14362f0ffcbde5bac457f82c620daf8b180"
 };
