8f1dd5d24de9681dda352ee8312bad017c77f7e0
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-09-24 13:04:30 +0300

    add routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 import { UserRequestApiService } from './user-http/user-request-api.service';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SearchComponent } from './search/search.component';
+import { SearchReposComponent } from './search-repos/search-repos.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+// Defining routes
+const routes:Routes=[
+  {path:"home",component:UserProfileComponent},
+  {path:"search",component:SearchComponent},
+  {path:"search-repos",component:SearchReposComponent},
+  {path:"",redirectTo:"/home",pathMatch:"full"},
+  {path:'**',component:NotFoundComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    SearchComponent,
+    SearchReposComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [UserRequestApiService],
   bootstrap: [AppComponent]
